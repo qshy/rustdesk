af94480786263b828121bacca9a503943ab7673e
qshy <by514@qq.com>
2025-06-13 11:10:48 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1003,7 +1003,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rustdesk.pcbw.top:21114".to_owned()
 }
 
 #[inline]
